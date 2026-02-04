55824c61f7674a8c60f701e47db704b6cb803f4b
wxd <114514@gmail.com>
2026-02-04 17:11:23 +0800

    修改了组合逻辑和时序时序，复制了top模块

--- rtl/CU.v
+++ rtl/CU.v
@@ -3,11 +3,11 @@ module CU(
     input  wire [6:0] opcode,  // 指令[6:0]
     input  wire [2:0] funct3,  // 指令[14:12]
     input  wire [6:0] funct7,  // 指令[31:25]
-    output reg       we,       // 寄存器写使能
-    output reg       alu_src,  // ALU输入源选择 (0:寄存器, 1:立即数)
-    output reg [3:0] alu_ctrl  // 告诉ALU做什么运算 (加/减) 加0000 减0001
+    output wire       we,       // 寄存器写使能
+    output wire       alu_src,  // ALU输入源选择 (0:寄存器, 1:立即数)
+    output wire [3:0] alu_ctrl  // 告诉ALU做什么运算 (加/减) 加0000 减0001
 );
-    always @(*) begin(
+    always @(*) begin
         case(opcode)
             //R型
             7'b0110011:begin
@@ -35,8 +35,8 @@ module CU(
                 //ADDI
                     7'b000: begin
                         we = 1'b1;
-                      alu_src = 1'b0;
-                       alu_ctrl = 4'b0000;
+                        alu_src = 1'b0;
+                        alu_ctrl = 4'b0000;
                    end
                 endcase
             end
