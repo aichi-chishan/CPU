2adc5bbee9a567af8a39c73f62063de6031587ef
wxd <114514@gmail.com>
2026-02-04 16:09:18 +0800

    完成了CU

---
+++ rtl/CU.v
@@ -0,0 +1,52 @@
+//control unit 控制单元
+module CU(
+    input  wire [6:0] opcode,  // 指令[6:0]
+    input  wire [2:0] funct3,  // 指令[14:12]
+    input  wire [6:0] funct7,  // 指令[31:25]
+    output reg       we,       // 寄存器写使能
+    output reg       alu_src,  // ALU输入源选择 (0:寄存器, 1:立即数)
+    output reg [3:0] alu_ctrl  // 告诉ALU做什么运算 (加/减) 加0000 减0001
+);
+    always @(*) begin(
+        case(opcode)
+            //R型
+            7'b0110011:begin
+                case(funct3)
+                    //ADD和SUB
+                    3'b000:begin
+                        case(funct7)
+                            //ADD
+                            7'b0000000: begin
+                                we = 1'b1;
+                                alu_src = 1'b0;
+                                alu_ctrl = 4'b0000;
+                            end
+                            //SUB
+                            7'b0100000: begin
+                                we = 1'b1;
+                                alu_src = 1'b0;
+                                alu_ctrl = 4'b0001;
+                            end
+                        endcase
+                endcase
+            //I型
+            7'0010011：begin
+                case(funct3)
+                //ADDI
+                    7'b000: begin
+                        we = 1'b1;
+                      alu_src = 1'b0;
+                       alu_ctrl = 4'b0000;
+                   end
+                endcase
+            end
+            end
+        endcase
+    end
+endmodule
+
+
+
+
+
+ 
